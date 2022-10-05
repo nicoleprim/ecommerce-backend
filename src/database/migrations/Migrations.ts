316a9d80942d8fe0f0ff37b57457d6a46413318d
Nicole Prim <nicolepprim@gmail.com>
2022-10-05 09:34:43 -0300

    ajustes no migrations para renderizar os produtos com mais que três colunas

--- src/database/migrations/Migrations.ts
+++ src/database/migrations/Migrations.ts
@@ -11,26 +11,25 @@ class Migrations extends BaseDatabase {
                 DROP TABLE IF EXISTS ${ProductDatabase.TABLE_PRODUCTS};
                 DROP TABLE IF EXISTS ${OrderDatabase.TABLE_ORDERS};
                 DROP TABLE IF EXISTS products_orders_shopper;
-                
+                                
                 CREATE TABLE IF NOT EXISTS ${ProductDatabase.TABLE_PRODUCTS}(
-                    id INT PRIMARY KEY,
-                    name VARCHAR(255) NOT NULL,
-                    price DECIMAL(4,2) NOT NULL,
-                    qty_stock INT NOT NULL
+                id INT PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                price DECIMAL(4,2) NOT NULL,
+                qty_stock INT NOT NULL
                 );     
                 CREATE TABLE IF NOT EXISTS ${OrderDatabase.TABLE_ORDERS}(
-                    id VARCHAR(255) PRIMARY KEY,
-                    user_name VARCHAR(255) NOT NULL,
-                    delivery_date DATE NOT NULL
+                id VARCHAR(255) PRIMARY KEY,
+                user_name VARCHAR(255) NOT NULL,
+                delivery_date DATE NOT NULL
                 );
                 CREATE TABLE IF NOT EXISTS products_orders_shopper(
-                    product_id INT NOT NULL,
-                    order_id VARCHAR(255) NOT NULL,
-                    qty INT NOT NULL,
-                    FOREIGN KEY (product_id) REFERENCES ${ProductDatabase.TABLE_PRODUCTS}(id),
-                    FOREIGN KEY (order_id) REFERENCES ${OrderDatabase.TABLE_ORDERS}(id) 
+                product_id INT NOT NULL,
+                order_id VARCHAR(255) NOT NULL,
+                qty INT NOT NULL,
+                FOREIGN KEY (product_id) REFERENCES ${ProductDatabase.TABLE_PRODUCTS}(id),
+                FOREIGN KEY (order_id) REFERENCES ${OrderDatabase.TABLE_ORDERS}(id) 
                 );
-                
                 `)
             }
 
@@ -40,16 +39,16 @@ class Migrations extends BaseDatabase {
 
             const arraySuccess: any = []
 
-            const productsVerify = productsFile.map((product: any) => {
+            productsFile.map((product: any) => {
                 if (product.field5 || product.field6) {
-                    console.log("Os produtos não puderam ser inseridos. Por favor, verifique os dados informados")
-                    return arrayErrors.push(product)
+                    return arrayErrors.push(product);
                 } else {
-                    return arraySuccess.push(product)
+                    return arraySuccess.push(product);
                 }
             })
 
-            console.log(productsVerify)
+            console.log("Os produtos não puderam ser inseridos. Por favor, verifique os dados informados", arrayErrors)
+
             const insertData = async () => {
                 await BaseDatabase
                     .connection(ProductDatabase.TABLE_PRODUCTS)
